657c568cfb983b72132f338fca86fa9d1f449f58
Arnold Lara <lara.arnoldandres@gmail.com>
2021-07-24 17:05:07 -0500

    Vista basica de items

--- ml340/inventario/views.py
+++ ml340/inventario/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Specification, Manufacturer, Category, Network, Status, Item
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    #return HttpResponse("Hello World")
+    return render(request,"inventario/index.html")
 
 def create(request):
+    
     return HttpResponse("Inventario Create")
 
 def read(request):
-    return HttpResponse("Inventario Read")
+    items = Item.objects.all()
+    num_items = Item.objects.all().count()
+    context = {'items':items, 'num_items':num_items}
+    #return HttpResponse("Inventario Read")
+    return render(request,"inventario/read.html",context)
 
 def update(request):
     return HttpResponse("Inventario Update")
